ddab93942f0c03c2a827e240e9da03517fcaedc1
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-24 00:44:07 +0530

    Add New Contact & Edit Contact

--- Address_Book_System/AddressBook.cs
+++ Address_Book_System/AddressBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,10 +19,32 @@ namespace Address_Book_System
             ContactDetails contact = new ContactDetails(firstname,lastname,address,city,state,zip,phone,email);
             contactDetails.Add(contact);
         }
+        public void AddNewContact()
+        {
+            Console.WriteLine("Enter your First Name: ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name: ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Enter your Address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your City: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter your State: ");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter your Zipcode: ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number: ");
+            long phone = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your EmailID: ");
+            string email = Console.ReadLine();
+
+            AddContactDetails(firstname, lastname, address, city, state, zip, phone, email);
+        }
         public void DisplayContacts()
         {
             foreach (var contact in contactDetails)
             {
+                Console.WriteLine("**********************************************");
                 Console.WriteLine("First Name: " + contact.firstname);
                 Console.WriteLine("Last Name: " + contact.lastname);
                 Console.WriteLine("Address: " + contact.address);
@@ -30,8 +53,61 @@ namespace Address_Book_System
                 Console.WriteLine("ZipCode: " + contact.zip);
                 Console.WriteLine("Phone Number: " + contact.phone);
                 Console.WriteLine("Email ID: " + contact.email);
+                Console.WriteLine("**********************************************");
             }
-                
+            
+
+        }
+
+        public void EditContact(string name)
+        {
+           for (int i = 0; i < contactDetails.Count; i++) 
+            {
+                ContactDetails contact = contactDetails[i];
+                if (contact.firstname == name || contact.lastname == name)
+                {
+                    Console.WriteLine("\n Choose What you want to edit " +
+                        "\n 1. First Name \n 2 Last Name \n 3. Address \n 4. City \n 5. State \n 6. ZipCode \n 7. Phone Number \n 8. Email ID");
+                    int edit = Convert.ToInt32(Console.ReadLine());
+                    switch (edit)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter New First Name: ");
+                            contact.firstname = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter New Last Name: ");
+                            contact.lastname = Console.ReadLine();
+                            break;                           
+                        case 3:
+                            Console.WriteLine("Enter New Address: ");
+                            contact.address = Console.ReadLine();
+                            break;                          
+                        case 4:
+                            Console.WriteLine("Enter New City: ");
+                            contact.city = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter New State: ");
+                            contact.state = Console.ReadLine();
+                            break;  
+                        case 6:
+                            Console.WriteLine("Enter New ZipCode: ");
+                            contact.zip = Convert.ToInt32(Console.ReadLine());
+                            break;                            
+                        case 7:
+                            Console.WriteLine("Enter New Phone Number: ");
+                            contact.phone = Convert.ToInt32(Console.ReadLine());
+                            break;                            
+                        case 8:
+                            Console.WriteLine("Enter New Email Id: ");
+                            contact.email = Console.ReadLine();
+                            break;
+                    }
+
+                }
+
+            }
         }
     }
 }
--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Address_Book_System
+﻿using System.Xml.Linq;
+
+namespace Address_Book_System
 {
     internal class Program
     {
@@ -7,8 +9,27 @@
             Console.WriteLine(" ******Address Book****** ");
             AddressBook addressBook = new AddressBook();
 
-            addressBook.AddContactDetails("Hemant", "Sharma", "Shahdara", "Delhi", "Delhi", 110093, 7532026247, "hshemantsharma445@gmail.com");
+            
+            Console.WriteLine("\n1.Create Contact\n2.Add New Contact\n3.Edit Contact");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    addressBook.AddContactDetails("Hemant", "Sharma", "Shahdara", "Delhi", "Delhi", 110093, 7532026247, "hshemantsharma445@gmail.com");                   
+                    break;
+                case 2:
+                    addressBook.AddNewContact();
+                    break;
+                case 3:
+                    addressBook.AddContactDetails("Hemant", "Sharma", "Shahdara", "Delhi", "Delhi", 110093, 7532026247, "hshemantsharma445@gmail.com");
+                    Console.WriteLine("Enter name of the contact you wish to edit: ");
+                    string name = Console.ReadLine();
+                    addressBook.EditContact(name);
+                    break; 
+            }
             addressBook.DisplayContacts();
         }
+
     }
 }
